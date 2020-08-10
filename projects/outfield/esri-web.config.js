c1202cca1850b2e27a2df9a048a928c2f2641d97
Ryann Galea <ryann@futurerad.com>
2020-08-10 13:43:32 +1000

    initial commit

---
+++ projects/outfield/esri-web.config.js
@@ -0,0 +1,19 @@
+const ArcGISPlugin = require("@arcgis/webpack-plugin");
+/**
+ * Configuration items defined here will be appended to the end of the existing webpack config defined by the Angular CLI.
+ */
+module.exports = {
+  plugins: [
+    new ArcGISPlugin({
+      root: "./",
+      features: {
+        "3d": false
+      }
+    })
+  ],
+  node: {
+    process: false,
+    global: false,
+    fs: "empty"
+  }
+};
